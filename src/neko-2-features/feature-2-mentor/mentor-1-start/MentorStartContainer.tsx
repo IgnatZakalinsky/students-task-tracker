b6ce4243bce8640adb59c0f7036ad49863cba1e3
LilBudj <kinich2001@gmail.com>
2019-12-14 15:23:06 +0300

    redirect

--- src/neko-2-features/feature-2-mentor/mentor-1-start/MentorStartContainer.tsx
+++ src/neko-2-features/feature-2-mentor/mentor-1-start/MentorStartContainer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import MentorStart from "./MentorStart";
-import {connect, useDispatch, useSelector} from "react-redux";
-import {IState, setTasksCountAC, startSessionThunkCreator} from "../mentor-4-redux/mentorReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {setTasksCountAC, startSessionThunkCreator} from "../mentor-4-redux/mentorReducer";
 import {IAppStore} from "../../../neko-1-main/main-2-bll/store";
 
 const MentorStartContainer: React.FC = () => {
     const mentorState = useSelector((store: IAppStore) => store.session);
     const dispatch = useDispatch();
     const startSession = (count: number) => {
-        dispatch(startSessionThunkCreator(count))
+        dispatch(startSessionThunkCreator(count));
     };
     const setCount = (count: number) => {
         dispatch(setTasksCountAC(count))
