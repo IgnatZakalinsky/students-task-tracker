349cb7d5d15cf1e0fb668311fa80e0ecfdb53975
Ignat <ai73a@yandex.ru>
2019-12-15 20:09:02 +0300

    Updates

--- precache-manifest.e3900e487b95ee8950a2e98e5d41d111.js
+++ precache-manifest.e3900e487b95ee8950a2e98e5d41d111.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "33239bf7e5a90de3290782b7a5588f78",
+    "revision": "73b81a2b7cfe7273bcfa9d809f597552",
     "url": "/students-task-tracker/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/students-task-tracker/static/css/2.c726b981.chunk.css"
   },
   {
-    "revision": "ba6c187bf7890e732198",
+    "revision": "5043b4e11b980a535274",
     "url": "/students-task-tracker/static/css/main.da25c89f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/students-task-tracker/static/js/2.ce284076.chunk.js.LICENSE"
   },
   {
-    "revision": "ba6c187bf7890e732198",
-    "url": "/students-task-tracker/static/js/main.4ad0ea60.chunk.js"
+    "revision": "5043b4e11b980a535274",
+    "url": "/students-task-tracker/static/js/main.ef866946.chunk.js"
   },
   {
     "revision": "6545f35b93813eb03e4a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/students-task-tracker/precache-manifest.1d6bce1a40377c24f1c815d7fa061d3b.js"
+  "/students-task-tracker/precache-manifest.e3900e487b95ee8950a2e98e5d41d111.js"
 );
 
 self.addEventListener('message', (event) => {
