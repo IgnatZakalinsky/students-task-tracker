bc8d14dbc85a9c16c6f35d358f8b8065566f6923
Ignat <ai73a@yandex.ru>
2019-12-15 19:10:43 +0300

    Updates

---
+++ precache-manifest.1d6bce1a40377c24f1c815d7fa061d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33239bf7e5a90de3290782b7a5588f78",
+    "url": "/students-task-tracker/index.html"
+  },
+  {
+    "revision": "1106b9ff8c3992f2889f",
+    "url": "/students-task-tracker/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "ba6c187bf7890e732198",
+    "url": "/students-task-tracker/static/css/main.da25c89f.chunk.css"
+  },
+  {
+    "revision": "1106b9ff8c3992f2889f",
+    "url": "/students-task-tracker/static/js/2.ce284076.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/students-task-tracker/static/js/2.ce284076.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba6c187bf7890e732198",
+    "url": "/students-task-tracker/static/js/main.4ad0ea60.chunk.js"
+  },
+  {
+    "revision": "6545f35b93813eb03e4a",
+    "url": "/students-task-tracker/static/js/runtime-main.ae3baa1b.js"
+  }
+]);
